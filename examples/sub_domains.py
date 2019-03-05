b904d310c2e29b6f9fd0daa5c2d7bf49f475fcc2
rhodrin <rhodri.nelson@gmail.com>
2019-03-05 19:18:24 +0000

    Testing.

--- examples/sub_domains.py
+++ examples/sub_domains.py
@@ -1,51 +1,61 @@
 import numpy as np
 
-from devito import Grid
+from devito import Grid, Dimension
 from devito import SubDomain, SubDimension
 from devito.types import SubDomains
 
-from devito import TimeFunction, Eq, Constant, Operator
+from devito import Function, TimeFunction, Eq, Constant, Operator, solve
+
+from sympy.utilities.lambdify import lambdify, implemented_function
+from sympy.abc import z
 
 from math import floor
 
 n_domains = 10
-extent = np.zeros((n_domains,2,2))
+extent = np.zeros((n_domains,2,2), dtype=int)
 
 for j in range(0,extent.shape[0]):
-    extent[j,0,0] = j
-    extent[j,0,1] = j
-    extent[j,1,0] = j-floor(j/2) 
-    extent[j,1,1] = n_domains-1-floor(j/2)
+    extent[j,0,0] = j                          # xmin
+    extent[j,0,1] = j                          # xmax
+    extent[j,1,0] = j-floor(j/2)               # ymin
+    extent[j,1,1] = n_domains-1-j-floor(j/2)   # ymax
 
 class MyDomains(SubDomains):
     name = 'MyDomains'
     def define(self, dimensions):
         x, y = dimensions
-        xm = Constant(name='xm')
-        xM = Constant(name='xM')
-        ym = Constant(name='ym')
-        yM = Constant(name='yM')
-        xi = SubDimension.middle('xi', x, xm, xM)
-        yi = SubDimension.middle('yi', y, ym, yM)
-        return {x: ('middle', xi.symbolic_min, xi.symbolic_max),
-                y: ('middle', yi.symbolic_min, yi.symbolic_max)}
+        #xmn = implemented_function('xmn', lambda z: extent[z,0,0])
+        #xmx = implemented_function('xmx', lambda z: extent[z,0,1])
+        #ymn = implemented_function('ymn', lambda z: extent[z,1,0])
+        #ymx = implemented_function('ymx', lambda z: extent[z,1,1])
+        #lxmn = lambdify(self.indices, xmn(self.indices))
+        #lxmx = lambdify(self.indices, xmx(self.indices))
+        #lymn = lambdify(self.indices, ymn(self.indices))
+        #lymx = lambdify(self.indices, ymx(self.indices))
+        #return {x: ('middle', self.extent[n,0,0], n_domains-1-self.extent[n,0,1]),
+                #y: ('middle', self.extent[n,1,0], n_domains-1-self.extent[n,1,1])}
+        return {x: ('middle', self.extent[0,0,0], n_domains-1-self.extent[0,0,1]),
+                y: ('middle', self.extent[0,1,0], n_domains-1-self.extent[0,1,1])}
     
 subs = MyDomains(n_domains=n_domains, extent=extent)
     
 grid = Grid(extent=(10, 10), shape=(10, 10), origin=(0, 0), subdomains = (subs))
-
-f = TimeFunction(name='f', grid=grid)
-
 x, y = grid.dimensions
 
-#eq = Eq(f, x, subdomain = grid.subdomains['MyDomains'])
+#f = TimeFunction(name='f', grid=grid)
+#f.data[:] = 0.0
+#eq = Eq(f.dt, 1, subdomain = grid.subdomains['MyDomains'])
+#stencil = solve(eq, f.forward)
+#op = Operator(Eq(f.forward, stencil))
+#op.apply(time_m=0, time_M=2, dt=1)
+
+f = Function(name='f', grid=grid)
+f.data[:] = 0.0
 eq = Eq(f, 1, subdomain = grid.subdomains['MyDomains'])
-
-#from IPython import embed
-#embed()
-
 op = Operator(eq)
-    
-#from IPython import embed
-#embed()
+op.apply()
+
 print(op.ccode)
+
+from IPython import embed
+embed()
