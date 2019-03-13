837167f77c49bf4a4d47f399525168d5f28ba44a
Fabio Luporini <f.luporini12@imperial.ac.uk>
2019-03-13 16:37:31 +0100

    ci,tests: Drop some MPI tests from Travis (but keep them on pipelines)

--- tests/test_mpi.py
+++ tests/test_mpi.py
@@ -1291,7 +1291,7 @@ class TestIsotropicAcoustic(object):
         ((60, 70), 'OT2', 8, 10, False, 351.217, 867.420, 405805.482, 239444.952),
         ((60, 70, 80), 'OT2', 12, 10, False, 153.122, 205.902, 27484.635, 11736.917)
     ])
-    @pytest.mark.parallel(mode=[(4, 'basic'), (4, 'diag'), (4, 'overlap'),
+    @pytest.mark.parallel(mode=[(4, 'basic', True), (4, 'diag'), (4, 'overlap', True),
                                 (4, 'overlap2'), (4, 'full')])
     def test_adjoint_F(self, shape, kernel, space_order, nbpml, save,
                        Eu, Erec, Ev, Esrca):
