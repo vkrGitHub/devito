6bf28626e2df66b1cc109c6c67864b5adc7936bb
victor.ramalho <vik_koehne@hotmail.com>
2020-01-07 15:48:47 -0200

    a

---
+++ examples/seismic/tutorials/01_modelling.py
@@ -0,0 +1,22 @@
+import numpy as np
+
+#NBVAL_IGNORE_OUTPUT
+from examples.seismic import Model, plot_velocity
+
+# Define a physical size
+shape = (101, 101)  # Number of grid point (nx, nz)
+spacing = (10., 10.)  # Grid spacing in m. The domain size is now 1km by 1km
+origin = (0., 0.)  # What is the location of the top left corner. This is necessary to define
+# the absolute location of the source and receivers
+
+# Define a velocity profile. The velocity is in km/s
+v = np.empty(shape, dtype=np.float32)
+v[:, :51] = 1.5
+v[:, 51:] = 2.5
+
+# With the velocity and model size defined, we can create the seismic model that
+# encapsulates this properties. We also define the size of the absorbing layer as 10 grid points
+model = Model(vp=v, origin=origin, shape=shape, spacing=spacing,
+              space_order=2, nbl=10)
+
+plot_velocity(model)
